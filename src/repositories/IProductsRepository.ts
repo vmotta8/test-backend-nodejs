24cc9d85518861a816c384d2439c91dc087be6e2
Vinicius Motta <viniciusmotta8@gmail.com>
2021-01-26 17:02:40 -0300

    feat: add mongo products repository

---
+++ src/repositories/IProductsRepository.ts
@@ -0,0 +1,11 @@
+import { Product } from '@/entities/Product'
+
+export interface IProductsRepository {
+  save (product: Product): Promise<void>;
+  findOneByTitle (userId: string, title: string): Promise<Product>;
+  findByTitle (userId: string, title: string): Promise<any>;
+  findOneByCategory (userId: string, category: string): Promise<Product>
+  findByCategory (userId: string, category: string): Promise<any>;
+  findAll (userId: string): Promise<any>;
+  updateMany(userId: string, query: object, update: object): Promise<any>;
+}
---
+++ src/repositories/implementations/MongoProductsRepository.ts
@@ -0,0 +1,47 @@
+import { database } from '@/database'
+import { Product } from '@/entities/Product'
+import { IProductsRepository } from '@/repositories/IProductsRepository'
+
+export class MongoProductsRepository implements IProductsRepository {
+  async save (product: Product): Promise<void> {
+    const productCollection = database.getCollection('products')
+
+    await productCollection.insertOne(product)
+  }
+
+  async findOneByTitle (userId: string, title: string): Promise<Product> {
+    const productCollection = database.getCollection('products')
+    const result = await productCollection.findOne({ title: title, userId: userId })
+    return result
+  }
+
+  async findByTitle (userId: string, title: string): Promise<any> {
+    const productCollection = database.getCollection('products')
+    const result = await productCollection.find({ title: title, userId: userId })
+    return result
+  }
+
+  async findOneByCategory (userId: string, category: string): Promise<Product> {
+    const productCollection = database.getCollection('products')
+    const result = productCollection.findOne({ category: category, userId: userId })
+    return result
+  }
+
+  async findByCategory (userId: string, category: string): Promise<any> {
+    const productCollection = database.getCollection('products')
+    const result = productCollection.find({ category: category, userId: userId })
+    return result
+  }
+
+  async findAll (userId: string): Promise<any> {
+    const productCollection = database.getCollection('products')
+    const result = productCollection.find({ userId: userId })
+    return result
+  }
+
+  async updateMany (userId: string, query: object, update: object): Promise<any> {
+    const productCollection = database.getCollection('products')
+    const result = await productCollection.updateMany({ ...query, ...{ userId: userId } }, { $set: update })
+    return result
+  }
+}
